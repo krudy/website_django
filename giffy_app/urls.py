e3ceb919a696248c17fba6707d81cce226d30c4b
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-12-08 11:45:59 +0100

    added uploading images

--- giffy_app/urls.py
+++ giffy_app/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path
 from .views import IndexView
+from django.contrib.auth.views import LoginView,LogoutView
+from .views import IndexView, SignupView, custom_logout, UploadImageView
+
+app_name = "giffy_app"
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    
+    path('login/', LoginView.as_view( 
+        template_name="giffy_app/login.html",
+        next_page="giffy+app:index",
+        redirect_authenticated_user=True,
+        ),
+         name='login',),
+    
+    path('logout/', custom_logout,
+         name='logout'),
+    
+    path('signup/', SignupView.as_view(), name='signup'),
+    
+    path('upload/', UploadImageView.as_view(), name='upload'),
 ]
--- giffy_app/views.py
+++ giffy_app/views.py
@@ -1,9 +1,80 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import datetime
+from .form import UploadImageForm
+
 from django.views import View 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Image
+
+def custom_logout(request):
+    logout(request)
+    return redirect('giffy_app:index')
 
 class IndexView(View):
     def get(self, request):
         images = Image.objects.order_by('upload_date')
         return render(request, 'giffy_app/index.html', {'images': images})
     
+class SignupView(View):
+    def get(self, request):
+        return render(request,
+                      'giffy_app/signup.html',
+                      {
+                          'form': UserCreationForm()
+                      })
+    
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.data['username']
+            password = form.data['password1']
+            user = authenticate(
+                request,
+                username=username,
+                password=password,
+                )
+            if user:
+                login(request, user)
+                return redirect('/')
+        else:
+            return render(request,
+                          'giffy_app/signup.html',
+                          {
+                              'form': form,
+                          })
+
+class UploadImageView(LoginRequiredMixin, View):
+    login_url = '/login/'
+    
+    def get(self, request):
+        return render(request,
+                      'giffy_app/upload.html',
+                      {
+                          'form': UploadImageForm()
+                      })
+        
+    def post(self, request):
+        user = request.user
+        
+        if not user.is_authenticated:
+            raise Exception('You must be logged in')
+        form = UploadImageForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            image = Image(
+                title = form.data['title'],
+                image = form.files["image"],
+                upload_date = datetime.now(),
+                uplouded_by = user,
+            )
+            image.save()
+            return redirect('/')
+        return render(request,
+                      'giffy_app/upload.html',
+                      {
+                          'form': form,
+                          'error': 'Invalid form data'
+                      })
